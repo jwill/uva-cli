338004cfd2eb437dce3ef35d69887cbd94e50fa2
Lucas Tan <tbhtan3@gmail.com>
2011-09-05 15:49:46 -0400

    Added ssize_t for Windows

--- Common.h
+++ Common.h
@@ -1,8 +1,8 @@
 /**
-Common defines
-
-@author Lucas Tan
-*/
+ * Common defines 
+ *
+ * @author Lucas Tan
+ */
 
 #ifndef COMMON_H
 #define COMMON_H
@@ -11,9 +11,13 @@ Common defines
     #define OSWIN
     #include <winsock2.h>
     typedef SOCKET sock_t;
+    typedef SSIZE_T ssize_t;
 #else
     typedef int sock_t;
+    #include <unistd.h>
 #endif
+
+#define SSIZE_T_MAX ((ssize_t)(((size_t)-1) >> 1))
 
 #endif // COMMON_H
 
